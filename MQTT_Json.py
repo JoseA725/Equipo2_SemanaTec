7939980f8550ecf239c030ae31ffc29cc16a1535
Alan de la Cruz <alan09delacruz@gmail.com>
2021-10-29 09:57:43 -0500

    cambie los nombres del diccionario y igual a las variables de lectura, ya no truena

--- MQTT_Json.py
+++ MQTT_Json.py
@@ -23,7 +23,7 @@ def on_connect( client, userdata, flags, rc):
     # Subscribe Topic from here
     # Aprovechando que se conectó, hacemos un subscribe a los tópicos
     #client.subscribe("fjhp6619mxIn")
-    client.subscribe("fjhp6619mxIn")
+    client.subscribe("equipo2SemanaTecIn")
 
 # Callback Function on Receiving the Subscribed Topic/Message
 # Cuando nos llega un mensaje a los tópicos suscritos, se ejecuta
@@ -63,20 +63,20 @@ def on_message( client, userdata, msg):
 def envia_dispositivo():
     dispositivo = input('Nombre del dispositivo:')
     sensor_actuador = input('Sensor del dispositivo:')
-    accion = int(input('Acción:'))
-    dicSalida = {'dispositivo':dispositivo,'sens_act':sensor_actuador,'accion':accion}
+    accion = int(input('Acción (int):'))
+    dicSalida = {'dispositivo':dispositivo,'tipo':sensor_actuador,'dato':accion} #cambie el nombre de los dirccionarios para empatar
     salidaJson = json.dumps(dicSalida)
     print('Salida Json:', salidaJson)
-    client.publish("tc1004b/profe/dispositivos",salidaJson)
+    #client.publish("tc1004b/profe/dispositivos",salidaJson)
     #time.sleep(4)
-    client.publish("fjhp6619mxIn",salidaJson)
+    client.publish("equipo2SemanaTecIn",salidaJson)
 
 # Envía un mensaje de prueba para que se procese en la llegada de mensajes
 def mensaje_debug():
     salida = '{"dispositivo":"Debug1","tipo":"Debug_Tipo","dato":5}'
     input('Mensaje de prueba: ' + salida)
-    client.publish("tc1004b/profe/dispositivos",salida)
-    client.publish("fjhp6619mxIn",salida)
+    #client.publish("tc1004b/profe/dispositivos",salida)
+    client.publish("equipo2SemanaTecIn",salida)
 
 #def consulta():
 #    mycursor.execute('DESCRIBE PyLog2')
